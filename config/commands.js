e60596d2400c9c2c101208a544236a6a2ec24570
小太 <nospam@xiaotai.org>
2014-06-14 01:44:28 +1000

    Add /customcolour

--- config/commands.js
+++ config/commands.js
@@ -1138,6 +1138,15 @@ var commands = exports.commands = {
 	 * Custom commands
 	 *********************************************************/
 
+	cc: 'customcolour',
+	customcolour: function (target, room) {
+		var targets = target.split(',');
+		if (targets.length < 2) return this.sendReply("/customcolour OR /cc [colour], [message] - Outputs a message in a custom colour. Requires: " + Users.getGroupsThatCan('customcolour', room).join(" "));
+		if (!this.can('customcolour', room) || !this.canBroadcast('!cc')) return false;
+
+		this.sendReply('|raw|<font color="' + targets[0].toLowerCase().replace(/[^#a-z0-9]+/g, '') + '">' + Tools.escapeHTML(targets.slice(1).join(",")) + '</font>');
+	},
+
 	customavatars: 'customavatar',
 	customavatar: (function () {
 		const script = (function () {/*
